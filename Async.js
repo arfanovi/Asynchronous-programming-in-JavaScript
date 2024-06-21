91c121b7f9d777f2a4ae415dd2a32b02ed347d9d
arfanovi <arfanovi.gub1@gmail.com>
2024-06-22 03:25:29 +0600

    Setup & Declare Normal Js Func

---
+++ Async.js
@@ -0,0 +1,30 @@
+// Suppose Normal Js Code 
+function Person1(){
+    console.log('Jhon');
+}
+
+function Person2(){
+    console.log('Ovi')
+}
+
+function Person3(){
+    console.log('Doe')
+}
+
+Person1();
+Person2();
+Person3();
+
+
+/*
+const Person = [
+    {Name: 'Ovi', Address: 'Dhaka Bangladesh', Chamber: 'Mirpur', Message: 'arfanovi@gmail.com'},
+    {Name: 'Jhon', Address: 'Rangpur Bangladesh', Chamber: 'Panchagarh', Message: 'jhon@gmail.com'}
+
+]
+
+// Declare Function 
+function getPost() {
+
+}
+*/
